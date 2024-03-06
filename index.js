812cef49a4314377a520389dfac0ff2abc9f8655
Andres David <kyfunky1386@gmail.com>
2024-03-05 21:04:17 -0500

    repositorio

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require ('express')
+
+
+const app  = express
+const puerto = 8000
+
+
+app.get ('/', (req,res) => {
+    res.send('Hola Mundo!')
+
+})
+
+
+app.listen (puerto, ()=> {
+    console.log(`El servidor está escuchando en http://localhost:${puerto}`);
+})
